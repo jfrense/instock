7b31b2557e1c6cb81d8c2ba159453d14d23a07e2
jfrense <jlfrense@gmail.com>
2024-02-23 20:08:35 -0500

    added example Order List

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import OrderList from '../components/OrderList.vue';
+
+
+</script>
+
+<template>
+    <div class="min-w-full min-h-full flex flex-row ">
+        <div class="w-1/3 bg-[#e8eaed]">
+        <OrderList />
+        </div>
+        <div class="w-2/3 border ">
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
